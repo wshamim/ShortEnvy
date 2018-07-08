be0f405e0837411055fc9f983cc60283e194958b
Wahaj Shamim <wahaj@southbanksoftware.com>
2018-07-09 08:02:29 +1000

    Minor fix for host

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const app = require('./server/app.js'),
-  PORT = process.env.PORT || 8000;
+  PORT = process.env.PORT || 8443,
+  HOST = process.env.HOST || '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`REST API running on ${PORT}!`);
+app.listen(PORT, HOST, () => {
+  console.log(`REST API running on ${HOST}:${PORT}!`);
 });
