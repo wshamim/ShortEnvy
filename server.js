fca8888a69334c78b306862dd3b785e0e7bacec5
Wahaj Shamim <wahaj@southbanksoftware.com>
2018-07-09 09:23:40 +1000

    Minor Changes for Local and Prod env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = require('./server/app.js'),
-  PORT = process.env.PORT || 8443,
-  HOST = process.env.HOST || '0.0.0.0';
+  PORT = process.env.PORT || 8000,
+  HOST = process.env.HOST || 'localhost';
 
 app.listen(PORT, HOST, () => {
   console.log(`REST API running on ${HOST}:${PORT}!`);
